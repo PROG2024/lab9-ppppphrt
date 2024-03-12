852ed3a366acf6a33a391e51b6edfe8b6b9f060f
ppppphrt <ppppphrt@ppppphrts-MacBook-Air.local>
2024-03-12 16:06:06 +0700

    fixed2

--- test_circle.py
+++ test_circle.py
@@ -11,18 +11,22 @@ import unittest
 import sys
 
 from circle import Circle
-
+import math
 
 class TestCircle(unittest.TestCase):
     def test_add_area_typical(self):
-        c1 = Circle(1)
-        c2 = Circle(2)
-        self.assertEqual(c1.add_area(c2), 12.566370614359172)
+        c1 = Circle(3)
+        c2 = Circle(4)
+        c3 = c1.add_area(c2)
+        self.assertEqual(c3.get_radius(), 5)
+        self.assertAlmostEqual(c3.get_area(), math.pi * 25)
 
     def test_add_area_edge_case(self):
         c1 = Circle(0)
         c2 = Circle(2)
-        self.assertEqual(c1.add_area(c2), 12.566370614359172)
+        c3 = c1.add_area(c2)
+        self.assertEqual(c3.get_radius(), 2)
+        self.assertEqual(c3.get_area(), 12.566370614359172)
 
     def test_circle_constructor(self):
         with self.assertRaises(ValueError):
